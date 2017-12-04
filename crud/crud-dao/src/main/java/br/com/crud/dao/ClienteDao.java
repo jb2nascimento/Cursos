36e02924369887521ef0c123bbf393a524f587aa
DanielFerreira Barbosa <danielfb236@gmail.com>
2017-12-04 21:39:14 -0200

    DFB_0412

---
+++ crud/crud-dao/src/main/java/br/com/crud/dao/ClienteDao.java
@@ -0,0 +1,30 @@
+package br.com.crud.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import br.com.cliente.Cliente;
+import br.com.crud.IClienteDao;
+
+
+public class ClienteDao implements IClienteDao {
+	
+	private EntityManager gerenciaBancoDeDados;
+	
+	public ClienteDao (EntityManager entity) {
+		
+		this.gerenciaBancoDeDados = entity;
+		
+	}
+
+	
+	@SuppressWarnings("unchecked")
+	public List<Cliente> selectAll() {
+		Query query = this.gerenciaBancoDeDados.createQuery("select c from Cliente c");
+		List<Cliente> todoOsRegistros = query.getResultList();
+		return todoOsRegistros;
+	}
+	
+}
--- crud/crud-web/src/main/java/br/com/beans/ClienteBean.java
+++ crud/crud-web/src/main/java/br/com/beans/ClienteBean.java
@@ -1,9 +1,28 @@
 package br.com.beans;
 
+import java.util.List;
+
+import br.com.cliente.Cliente;
+import br.com.crud.service.CrudFactory;
+import br.com.crud.service.IClienteService;
+import lombok.Data;
+
+@Data
 public class ClienteBean {
 
-	public String executar() {
-		return "";
+	private List<Cliente> todosOsClientes;
+	
+	private IClienteService clienteService;
+	
+	public ClienteBean() {
+		
+		carregarClientes();
 	}
-
+	
+	public void carregarClientes() {
+		
+		clienteService = CrudFactory.getInstance();
+		todosOsClientes = clienteService.recuperarTodosClientes();
+		
+	}
 }
